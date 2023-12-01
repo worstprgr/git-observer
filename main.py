f98fe1120b9699cb72540db1ac18ef00c1511427
Pitcher <pitcher.seven@mailbox.org>
2023-12-01 13:25:16 +0100

    8 - Move transport objects to separate file
    The class Commit was located in GitObserver
    If applied a new file is created that contains Commit class and newly created Observation class wrapping a list[Commit] and a topic/name.
    This also contains minor changes on fields, that are in fact not mandatory
    * Link/hash
    * Branch name
    Please note this makes origin optional as well since only used for optional built of commit_link

--- main.py
+++ main.py
@@ -4,21 +4,39 @@ import datetime
 import time
 from argparse import ArgumentParser
 
-def call_commandline(args):
+
+def call_commandline(config):
+    """
+    Calls the command line tool GitObserver using
+    passed config in an endless loop paused for 60s per run
+    :param config: configuration of command line tool
+    :return: None
+    """
     print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} Starting git log observer...")
-    observer = GitObserver(args)
+    observer = GitObserver(config)
     while 1:
         observer.run()
         time.sleep(60)
 
+
 def parse_arguments():
-     parser = ArgumentParser(allow_abbrev=True)
-     parser.add_argument('-o', '--origin', metavar='origin', help='Origin path to build commit link for output', required=True)
-     parser.add_argument('-f', '--filepath', help='Git root to be observed', action='store', required=True)
-     parser.add_argument('-lf', '--logfolders', metavar='Folder', type=str, nargs='+', help='Folder(s) to be observed', required=True)
-     parser.add_argument('-ig', '--ignore', metavar='Author', type=str, nargs='+', help='Author name to be ignored')
-     return parser.parse_args()
+    """
+    Parses the given arguments passed by caller and
+    returns the parsed result
+    :return:
+    """
+    parser = ArgumentParser(allow_abbrev=True)
+    parser.add_argument('-o', '--origin', metavar='origin', default='',
+                        help='Origin path to build commit link for output', required=False)
+    parser.add_argument('-f', '--filepath', action='store',
+                        help='Git root to be observed', required=True)
+    parser.add_argument('-lf', '--logfolders', metavar='Folder', type=str, nargs='+',
+                        default='.', help='Folder(s) to be observed', required=False)
+    parser.add_argument('-ig', '--ignore', metavar='Author', type=str, nargs='+',
+                        help='Author name to be ignored')
+    return parser.parse_args()
+
 
 if __name__ == '__main__':
-       args = parse_arguments()
-       call_commandline(args)
+    parsed_config = parse_arguments()
+    call_commandline(parsed_config)
