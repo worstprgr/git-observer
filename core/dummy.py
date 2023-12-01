cf89365fb7d2c8a3a0216e55fa5ce144596f71b7
worstprgr <adam@seishin.io>
2023-12-01 22:11:47 +0100

    Added unit test examples

---
+++ core/dummy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+This file contains dummy functions, for testing the unit test ability and CI-Integration.
+"""
+
+
+def addition(a: int, b: int) -> int:
+    return a + b
