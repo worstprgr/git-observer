7af9e194d7683ae858fa134e5f45a3a463e0ed7f
worstprgr <adam@seishin.io>
2023-12-01 22:11:47 +0100

    Added unit test examples

---
+++ core/tests/test_init.py
@@ -0,0 +1,56 @@
+import unittest
+import core.dummy
+
+
+class MyTestCase(unittest.TestCase):
+    """
+    This is a basic boilerplate how unit tests in Python are structured.
+
+    Docs:
+    https://docs.python.org/3/library/unittest.html
+    """
+    def test_something1(self):
+        """
+        Assert if a and b are equal.
+        """
+        the_source_of_truth = True
+        method_to_test = True
+
+        self.assertEqual(the_source_of_truth, method_to_test)
+
+    def test_something2(self):
+        """
+        Assert if a specific exception is raised.
+        """
+        # List with 3 items
+        a_list = ['Hello', 'Hi', 'Morning']
+
+        # Function, that accesses the 6th item in the list (which does not exist)
+        def provoke_index_error(test_list): return test_list[5]
+
+        # The assertion happens inside a `context manager`.
+        # This allows to encapsulate the error.
+        # Any other approach would trigger the exception before an assertion.
+        # So in that case, we want to assert if an `IndexError` correctly appears.
+        with self.assertRaises(IndexError):
+            provoke_index_error(a_list)
+
+    def test_dummy(self):
+        """
+        Example how to work with imported modules.
+        """
+        c_dummy = core.dummy
+
+        test_var1 = 5
+        test_var2 = 10
+        expected_result = 15
+
+        scenario1 = c_dummy.addition(test_var1, test_var2)
+
+        self.assertEqual(expected_result, scenario1)
+
+
+# This runs all tests in this file, even if we implement multiple classes.
+# But you can run specific runs if you wish. You can call them inside the code like any other class.
+if __name__ == '__main__':
+    unittest.main()
