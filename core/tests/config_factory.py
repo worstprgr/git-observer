6add1a800fe165ef634be6032b94ddd9d0185540
worstprgr <adam@seishin.io>
2023-12-12 15:33:09 +0100

    Adding UTs for core files
    Signed-off-by: worstprgr <adam@seishin.io>

--- core/tests/config_factory.py
+++ core/tests/config_factory.py
@@ -1,8 +1,32 @@
+import pathlib
+from configparser import ConfigParser
 from argparse import Namespace
+
 from core.config import ConfigHandler
+import core.paths as c_paths
 
 
 class ConfigFactory:
+    # Static object, for comparing keywords inside a namespace
+    test_keywords = [
+        '[Default]',
+        'origin',
+        'filepath',
+        'logfolders',
+        'ignore',
+        'show_viewer',
+        'descending'
+    ]
+
+    config_names_ini_defaults: dict = {
+        'origin': 'https://github.com/worstprgr/git-observer/commit/',
+        'filepath': c_paths.BASE_DIR,
+        'logfolders': ['.'],
+        'ignore': [],
+        'show_viewer': True,
+        'descending': True
+    }
+
     @staticmethod
     def create_defaults():
         """
@@ -14,3 +38,19 @@ class ConfigFactory:
         for key in ConfigHandler.config_name_defaults.keys():
             namespace.__dict__[key] = ConfigHandler.config_name_defaults[key]
         return namespace
+
+    def create_config_file(self, fp: str | pathlib.Path):
+        """
+        Installing a config file
+        """
+        config = ConfigParser()
+        config.add_section('Default')
+        for key in self.config_names_ini_defaults.keys():
+            value = self.config_names_ini_defaults[key]
+            if type(value) is list:
+                config['Default'][key] = str.join(', ', value)
+            else:
+                config['Default'][key] = str(value)
+
+        with open(fp, 'w') as ini:
+            config.write(ini)
