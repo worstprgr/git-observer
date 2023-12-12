e7904865dea64aacdc59eba76efa8376a774a991
worstprgr <adam@seishin.io>
2023-12-12 15:22:25 +0100

    Moved test_init.py to boilerplate.py Since we have now real UTs, the inital file is no longer needed. It now acts as a boilerplate for new unit tests.

--- core/tests/boilerplate.py
+++ core/tests/boilerplate.py
@@ -1,5 +1,9 @@
 import unittest
-import core.dummy
+
+
+# Test function
+def addition(a: int, b: int) -> int:
+    return a + b
 
 
 class MyTestCase(unittest.TestCase):
@@ -11,23 +15,31 @@ class MyTestCase(unittest.TestCase):
     """
     def test_something1(self):
         """
+        Test Description (Mandatory)
         Assert if a and b are equal.
         """
+        # Given
         the_source_of_truth = True
         method_to_test = True
 
+        # When
+
+        # Then
         self.assertEqual(the_source_of_truth, method_to_test)
 
     def test_something2(self):
         """
+        Test Description (Mandatory)
         Assert if a specific exception is raised.
         """
-        # List with 3 items
+        # Given
         a_list = ['Hello', 'Hi', 'Morning']
 
+        # When
         # Function, that accesses the 6th item in the list (which does not exist)
         def provoke_index_error(test_list): return test_list[5]
 
+        # Then
         # The assertion happens inside a `context manager`.
         # This allows to encapsulate the error.
         # Any other approach would trigger the exception before an assertion.
@@ -37,16 +49,18 @@ class MyTestCase(unittest.TestCase):
 
     def test_dummy(self):
         """
+        Test Description (Mandatory)
         Example how to work with imported modules.
         """
-        c_dummy = core.dummy
-
+        # Given
         test_var1 = 5
         test_var2 = 10
         expected_result = 15
 
-        scenario1 = c_dummy.addition(test_var1, test_var2)
+        # When
+        scenario1 = addition(test_var1, test_var2)
 
+        # Then
         self.assertEqual(expected_result, scenario1)
 
 
