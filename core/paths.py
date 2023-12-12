6b3e7bbd1718cbc4b0c51ce64f0b7bfcd492372b
worstprgr <adam@seishin.io>
2023-12-12 15:30:51 +0100

    Refactoring for preparing the files for UTs ### config.py - replaced the hard coded path to `conf.ini` with a dynamic path - fixed typo - added a todo, for separating the parse from the other logic inside the method
    ### main.py
    - PEP8: moved project imports under lib imports
    - Todo: Discuss if a print() should be replaced by the logger
    
    ### paths.py
    - added a new folder `static/unittest`
    - added a new file `conf.ini`
    - organized files for UTs in a separate section
    
    ### unittestutils.py
    - added more helper methods
    - added a dummy namespace container

--- core/paths.py
+++ core/paths.py
@@ -11,8 +11,15 @@ BASE_DIR: str = path_utils.get_base_dir()
 CORE_DIR: str = BASE_DIR + '/core'
 STATIC_DIR: str = BASE_DIR + '/static'
 LOG_DIR: str = BASE_DIR + '/logs'
+STATIC_UT_DIR: str = STATIC_DIR + '/unittest'
 
 # Files
-GITLOG_DUMMY: str = STATIC_DIR + '/gitlog-dummy.txt'
-GITLOG_DUMMY_REDUNDANT: str = STATIC_DIR + '/gitlog-dummy-redundant.txt'
+CONFIG_INI: str = BASE_DIR + '/conf.ini'
 LOG_FILE = lambda a: LOG_DIR + f'/log_{a}.log'
+
+# Unittest
+BUNNY_FILE: str = STATIC_UT_DIR + '/bunny.txt'
+LOG_FILE_DUMMY: str = STATIC_UT_DIR + '/dummy-log.txt'
+GITLOG_DUMMY: str = STATIC_UT_DIR + '/gitlog-dummy.txt'
+GITLOG_DUMMY_REDUNDANT: str = STATIC_UT_DIR + '/gitlog-dummy-redundant.txt'
+CONF_INI_DUMMY: str = STATIC_UT_DIR + '/conf_dummy.ini'
