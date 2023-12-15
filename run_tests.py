af487c692c3ae764d14c698650176fdb9d929172
worstprgr <adam@seishin.io>
2023-12-15 03:47:29 +0100

    Testing Pytest Plans changed, see: https://github.com/worstprgr/git-observer/issues/54#issuecomment-1857183592
    Signed-off-by: worstprgr <adam@seishin.io>

--- run_tests.py
+++ run_tests.py
@@ -9,6 +9,7 @@ Features:
 """
 import subprocess
 import os
+import pathlib
 import typing
 
 
@@ -17,19 +18,30 @@ class RunTests:
         self.search_pattern: str = 'tests'
         self.root_dir: str = '.'
         self.root_dir_content: list = next(os.walk('.'))[1]
-        self.git_ignore_file: str = '.gitignore'
-        self.ignored_folders: list = ['.git', '.github']  # Add more folders to ignore, that aren't in your .gitignore
+        self.git_ignore_file: str = self._conv_to_path('.gitignore')
+
+        # Add more folders to ignore, that aren't in your .gitignore
+        self.ignored_folders: list = ['.git', '.github']
         self.allowed_folders: list = []
         self.all_test_folders: list = []
         self.unit_test_cmd: list = lambda a: ['python', '-m', 'unittest', 'discover', '-s', a]
 
-    def main(self):
+    def main(self, debug: bool = False) -> None:
+        """
+        Calls all needed methods, to execute found unit tests.
+        :param debug: If true, it won't execute any UTs. Instead, it displays all found directories.
+        :return: None
+        """
         print('Gathering Unit Test Folders')
         self.load_git_ignore_folders()
         self.get_allowed_folders()
         self.walk_allowed_folders()
-        print('Running All Unit Tests')
-        self.run_tests()
+
+        if not debug:
+            print('Running All Unit Tests')
+            self.run_tests()
+        else:
+            print(self.all_test_folders)
 
     def load_git_ignore_folders(self):
         """
@@ -57,21 +69,23 @@ class RunTests:
         Searches for folders, that are named like in `search_pattern`.
         :return: None
         """
-        def replace_backslash(string: str) -> str:
-            return string.replace('\\', '/') if '\\' in string else string
-
         def find_test_folders(folder: typing.Iterator) -> None:
             root: str
             for root, _, _ in folder:
                 if root.endswith(self.search_pattern):
-                    self.all_test_folders.append(replace_backslash(root))
+                    self.all_test_folders.append(self._conv_to_path(root))
 
         for x in self.allowed_folders:
             find_test_folders(os.walk(x))
 
     def run_tests(self):
+        x: pathlib.Path
         for x in self.all_test_folders:
-            subprocess.run(self.unit_test_cmd(x))
+            subprocess.run(self.unit_test_cmd(str(x)))
+
+    @staticmethod
+    def _conv_to_path(fp: str) -> pathlib.Path:
+        return pathlib.Path(fp)
 
 
 if __name__ == '__main__':
